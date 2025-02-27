7290270520134fff4174a9b4b98fcf7a802e550a
thadimariv <thadimariv@i2r.a-star.edu.sg>
2025-02-27 17:02:04 +0800

    stable python lsp

--- lua/yeshas/plugins/lsp/mason.lua
+++ lua/yeshas/plugins/lsp/mason.lua
@@ -27,27 +27,26 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "tsserver",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "svelte",
-        "lua_ls",
-        "graphql",
-        "emmet_ls",
-        "prismals",
+        -- "html",
+        -- "cssls",
+        -- "tailwindcss",
+        -- "svelte",
+        -- "lua_ls",
+        -- "graphql",
+        -- "emmet_ls",
+        -- "prismals",
         "pyright",
       },
     })
 
     mason_tool_installer.setup({
       ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint",
-        "eslint_d",
+        -- "prettier", -- prettier formatter
+        --"stylua", -- lua formatter
+        --"isort", -- python formatter
+        --"black", -- python formatter
+        --"pylint",
+        --"eslint_d",
       },
     })
   end,
